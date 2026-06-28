483cc199389e981540f16727faf58ea328ef2448
Simon Hörtzsch <simon@hoertzsch.de>
2026-06-29 01:11:05 +0200

    test: add truth table ci

---
+++ csharp/tests/TruthTableTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿namespace TruthTableTests;
+
+using LogicExpressions;
+using TruthTable;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Evaluate_ReturnsAssignedVariableValue()
+    {
+        var variable = new Variable("a");
+
+        bool result = TruthExpressionEvaluator.Evaluate(variable, new Dictionary<string, bool>
+        {
+            ["a"] = true,
+        });
+
+        Assert.True(result);
+    }
+}
